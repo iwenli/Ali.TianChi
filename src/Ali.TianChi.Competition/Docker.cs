d1757c6d38b041366c570d02af99174573d3326b
cc <home@iwenli.org>
2020-04-01 11:09:58 +0800

    1

--- src/Ali.TianChi.Competition/Docker.cs
+++ src/Ali.TianChi.Competition/Docker.cs
@@ -64,9 +64,9 @@ namespace Ali.TianChi.Competition
                 WriteIndented = true
             });
 
-            //Console.WriteLine($"run success.");
-            //Console.WriteLine(result);
-            File.WriteAllText(resultJsonFullPath, result, Encoding.UTF8);
+            Console.WriteLine($"run success.");
+            Console.WriteLine(result);
+            File.WriteAllText(resultJsonFullPath, result, new UTF8Encoding(false));
         }
     }
 }
